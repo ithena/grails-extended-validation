e8f3dc822b0d7547866e45d2234badda4168af6e
houbie <houbie@832c1c66-4827-0410-8465-ccb17913cd09>
2011-02-16 10:21:52 +0000

    prepare for 1.1 release
    git-svn-id: https://svn.codehaus.org/grails-plugins/grails-extended-validation/trunk@68947 832c1c66-4827-0410-8465-ccb17913cd09

--- ExtendedValidationGrailsPlugin.groovy
+++ ExtendedValidationGrailsPlugin.groovy
@@ -8,7 +8,7 @@ class ExtendedValidationGrailsPlugin {
     // the plugin version
     def version = "1.0"
     // the version or versions of Grails the plugin is designed for
-    def grailsVersion = "1.3.50 > *"
+    def grailsVersion = "1.3.0 > *"
     // the other plugins this plugin depends on
     def dependsOn = [:]
     // resources that are excluded from plugin packaging
